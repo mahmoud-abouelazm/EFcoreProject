f9478bd05a2ac45e2797eb71f216d3deb3bd79cb
Mahmoud Reda <mahmoudreda20030@gmail.com>
2026-04-05 17:26:20 +0200

    style welcome form

--- EFcoreProject/Form1.Designer.cs
+++ EFcoreProject/Form1.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            this.btnSysAdmin = new Button();
+            this.btnInstructor = new Button();
+            btnDptMng = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 25.8000011F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(128, 73);
+            label1.Name = "label1";
+            label1.Size = new Size(502, 60);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome To ITI System";
+            label1.Click += label1_Click;
+            // 
+            // btnSysAdmin
+            // 
+            this.btnSysAdmin.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            this.btnSysAdmin.Location = new Point(158, 326);
+            this.btnSysAdmin.Name = "btnSysAdmin";
+            this.btnSysAdmin.Size = new Size(448, 59);
+            this.btnSysAdmin.TabIndex = 1;
+            this.btnSysAdmin.Text = "System Admin";
+            this.btnSysAdmin.UseVisualStyleBackColor = true;
+            // 
+            // btnInstructor
+            // 
+            this.btnInstructor.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            this.btnInstructor.Location = new Point(158, 173);
+            this.btnInstructor.Name = "btnInstructor";
+            this.btnInstructor.Size = new Size(448, 59);
+            this.btnInstructor.TabIndex = 2;
+            this.btnInstructor.Text = "Instructor";
+            this.btnInstructor.UseVisualStyleBackColor = true;
+            // 
+            // btnDptMng
+            // 
+            btnDptMng.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnDptMng.Location = new Point(158, 248);
+            btnDptMng.Name = "btnDptMng";
+            btnDptMng.Size = new Size(448, 59);
+            btnDptMng.TabIndex = 3;
+            btnDptMng.Text = "Department Manager";
+            btnDptMng.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(760, 423);
+            Controls.Add(btnDptMng);
+            Controls.Add(this.btnInstructor);
+            Controls.Add(this.btnSysAdmin);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Welcome";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button btnDptMng;
     }
 }
--- EFcoreProject/Form1.cs
+++ EFcoreProject/Form1.cs
@@ -6,5 +6,10 @@ namespace EFcoreProject
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
